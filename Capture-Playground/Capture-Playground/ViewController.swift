b6029569510b2edb1662d7599632baff236c8932
Hugo Prinsloo <hugo.prinsloo@icloud.com>
2019-08-08 14:46:35 +0300

    Added demo project

---
+++ Capture-Playground/Capture-Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capture-Playground
+//
+//  Created by Hugo Prinsloo on 2019/08/08.
+//  Copyright © 2019 Hugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
